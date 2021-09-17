87a64c25d715388ece05b8e208a01fefb3e158f9
kukuh <kukuh.baiquni@gmail.com>
2021-09-18 03:47:16 +0700

    add image assets

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,23 +2,20 @@ import { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Sidebar from 'components/side-bar'
 import Image from 'next/image'
-import logo from '../../public/images/els-head.png'
+import logo from '../../public/images/logo.png'
 
 function Layout({ children }) {
   return (
     <Fragment>
       <header className='w-full bg-white'>
-        <div className='h-20 w-full md:w-3/4 mx-auto bg-gray-200 flex items-center'>
-          <div>
-            <Image
-              alt='logo'
-              className='object-cover bg-rena'
-              height={70}
-              src={logo}
-              width={70}
-            />
-            <h3 className='text-gray-600'>Elfun Wiki</h3>
-          </div>
+        <div className='h-20 w-full md:w-3/4 mx-auto flex items-center px-4 py-2'>
+          <Image
+            alt='logo'
+            className='cursor-pointer'
+            height={70}
+            src={logo}
+            width={150}
+          />
         </div>
       </header>
       <div className='w-full md:w-3/4 mx-auto flex bg-gray-300'>
