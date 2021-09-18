53bcc34777746c4abdb31244960000ea08088dfa
kukuh <kukuh.baiquni@gmail.com>
2021-09-18 19:09:59 +0700

    draft sidebar fix

--- src/components/side-bar/index.jsx
+++ src/components/side-bar/index.jsx
@@ -61,13 +61,13 @@ function Sidebar() {
     <div className='bg-white p-4 border-r border-gray-300 border-solid'>
       <ul>
         {navigation.map(({ name, child }) => (
-          <li className={`py-2 px-4 font-black ${child.length ? '' : 'hover:bg-sky-400 hover:text-gray-200 rounded cursor-pointer'} mt-0.5 duration-200 text-gray-500 transition-all`} key={name}>
+          <li className={`py-1 px-4 font-black ${child.length ? '' : 'hover:bg-sky-400 hover:text-gray-200 rounded cursor-pointer'} mt-1 duration-200 text-gray-500 transition-all`} key={name}>
             {child.length ? (
               <Fragment>
                 {name}
-                <ul className='mt-1 z-10 relative'>
+                <ul>
                   {child.map((item) => (
-                    <li className='py-1.5 whitespace-nowrap group p-4 font-normal text-gray-500 hover:bg-sky-400 hover:translate-x-2 hover:text-gray-200 rounded cursor-pointer transition-all duration-300' key={item.name}>
+                    <li className='py-1.5 relative pl-2 z-10 whitespace-nowrap group font-normal text-gray-400 hover:translate-x-2 hover:text-sky-400 rounded cursor-pointer transition-transform duration-300 mt-0.5' key={item.name}>
                       {item.name}
                       {item.child?.length && <JobPath data={item.child} />}
                     </li>
--- src/components/side-bar/job-path.jsx
+++ src/components/side-bar/job-path.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types'
 
 function JobPath({ data = [] }) {
   return (
-    <ul className='absolute group-hover:opacity-100 opacity-0 -bottom-full py-2 px-2 rounded -right-3/4 bg-white drop-shadow-2xl border border-gray-200 border-solid group-hover:translate-x-10 transition-all duration-300'>
+    <ul className='absolute floatig-list z-10 hidden group-hover:flex flex-col group-hover:opacity-100 opacity-0 top-0 py-2 px-2 rounded left-1/2 bg-white drop-shadow-2xl border border-gray-200 border-solid group-hover:translate-x-10 transition-all duration-300'>
       {data.map((firstJob) => (
-        <li className='job-nesting text-gray-500 group hover:bg-sky-400 transition-all duration-300 hover:text-gray-200 rounded py-2 px-5 relative' key={firstJob.name}>
+        <li className='job-nesting text-gray-500 hover:bg-sky-400 transition-all duration-300 hover:text-gray-200 rounded py-2 px-5' key={firstJob.name}>
           {firstJob.name}
-          <div className='relative opacity-0'>
-            <ul className='absolute p-2 rounded bg-white drop-shadow-2xl border border-gray-200 border-solid'>
+          <div className='opacity-0 relative'>
+            <ul className='absolute z-20 p-2 rounded bg-sky-400 -right-36 top-0'>
               {firstJob.child.map((secondJob) => <li className='text-gray-500 hover:bg-sky-400 transition-all duration-300 hover:text-gray-200 rounded py-2 px-5' key={secondJob.name}>{secondJob.name}</li>)}
             </ul>
           </div>
