9183ffeb7b7fcdbd3f5d1aadae8e2a7c23d6aeba
kukuh <kukuh.baiquni@gmail.com>
2021-09-20 12:45:25 +0700

    remove warning from window is undefined

--- src/components/side-bar/characters.jsx
+++ src/components/side-bar/characters.jsx
@@ -8,13 +8,17 @@ import characters from './character-data'
 function Characters() {
   const [collapse, setCollapse] = useState(true)
 
+  const afterAnimation = (e) => {
+    console.log(e.target)
+  }
+
   return (
     <Fragment>
       <span className='px-2 py-1 flex items-center hover:text-white cursor-pointer text-gray-600 dark:text-gray-400 hover:bg-gray-400 dark:hover:bg-gray-600' onClick={() => setCollapse((old) => !old)}>
         <p className='mr-2'> Characters ({characters.length}) </p>
         <ChevronDownIcon className={`w-5 h-5 ${collapse ? 'transform -rotate-180' : 'transform rotate-0'} transition-transform duration-300`} />
       </span>
-      <ul className={`px-2.5 mt-1 ${collapse ? 'h-0' : 'h-[480px]'} transition-all duration-300`}>
+      <ul className={`px-2.5 mt-1 ${collapse ? 'h-0' : 'h-[480px]'} transition-all duration-300`} onAnimationEnd={afterAnimation}>
         {characters.map((item, index) => (
           <li className={`relative z-10 pl-1 ${collapse ? 'opacity-0' : 'opacity-100'} hover:text-white text-gray-600 dark:text-gray-400 dark:hover:text-white group ${item.hoverBgColor} hover:translate-x-2 cursor-pointer transition-transform-opacity duration-300`} key={item.name}>
             <Link href={`/character/${item.name.toLowerCase()}`}>
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react'
 
 function useDarkMode() {
-  const defaultTheme = typeof window !== 'undefined' ? (localStorage.theme === 'undefined' ? 'light' : localStorage.theme) : 'light'
+  const defaultTheme = typeof window !== 'undefined' && localStorage.theme
   const [theme, setTheme] = useState(defaultTheme)
 
   useEffect(() => {
     const root = window.document.documentElement
-    if (theme === 'dark') {
+    if (theme === 'dark' && theme) {
       root.classList.add('dark')
     } else {
       root.classList.remove('dark')
     }
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && theme) {
       localStorage.setItem('theme', theme)
     }
   }, [theme])
