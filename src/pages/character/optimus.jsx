9381dae578c7bfbd6342ddfa83c1716136f3f7b8
kukuh <kukuh.baiquni@gmail.com>
2021-09-26 16:57:07 +0700

    draft character detail

--- src/pages/character/optimus.jsx
+++ src/pages/character/optimus.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
 import Head from 'next/head'
 import Image from 'next/image'
 import { Fragment } from 'react'
 import CharacterDetail from 'layouts/character-details'
 import OptimusCut from '../../../public/images/example/optimus-skill-cut.png'
-import CrimsonRoseCut from '../../../public/images/example/crimson-rose-skill-cut.png'
-import CRCut from '../../../public/images/example/c-rose.png'
+import optimus from '../../../public/images/example/optimus.png'
 
 const avatar = 'https://firebasestorage.googleapis.com/v0/b/elfun-web-app.appspot.com/o/images%2Favatar%2Frose%2Foptimus.png?alt=media&token=5b1f9adb-fee0-4c1a-9cde-d86e7bdadbbc'
 
@@ -17,24 +18,44 @@ export default function Optimus() {
         <meta content='Elsword, MMORPG, Optimus, Metal Heart, Prime Operator, Rose' name='keywords' />
       </Head>
       <main className='text-gray-600 dark:text-gray-400 px-2 h-[2000px]'>
-        <div className='max-w-screen-2xl h-[400px] bg-gradient-to-r from-gray-700 flex justify-end relative p-3'>
+        <section className='max-w-screen-2xl h-[400px] bg-gradient-to-r from-gray-700 flex justify-end relative sm:p-3 mb-10'>
           <Image
             alt='Optimus Skin Cut'
             className='object-cover'
             src={OptimusCut}
           />
-          <div className='flex items-center absolute left-3 bottom-3'>
+          <div className='flex items-center absolute left-2 bottom-2 sm:left-3 sm:bottom-3'>
             <Image alt='Optimus' height={54} quality={100} src={avatar} width={54} />
             <h2 className='text-3xl font-semibold text-rose font-titillium ml-3'>Optimus</h2>
           </div>
-        </div>
+        </section>
 
-        <div className='p-2 max-w-full sm:max-w-3xl mx-auto'>
-          <p className='text-sm sm:text-xl text-center text-rose'>
-            The ecstasy you get when you design as much driving parts as you want,
-            since design was considered a weakness as a weapon, was indescribable!
-          </p>
-        </div>
+        <section className='bg-gray-700 flex relative justify-between p-3'>
+          <div className='max-w-full sm:max-w-3xl'>
+            <p className='text-sm sm:text-lg text-rose'>
+              The ecstasy you get when you design as much driving parts as you want,
+              since design was considered a weakness as a weapon, was indescribable!
+            </p>
+            <div className='sm:max-w-screen-lg overflow-x-auto'>
+              <div className='grid grid-cols-4 w-[700px] sm:max-w-lg'>
+                <div className='border border-gray-600 px-2 py-1 border-b-0'>Name</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 col-span-3'>Anna Testarosa</div>
+                <div className='border border-gray-600 px-2 py-1 border-b-0'>Class</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 col-span-3'>Optimus</div>
+                <div className='border border-gray-600 px-2 py-1 border-b-0'>Weapon</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 col-span-3'>Guns (Auto-guns), Robot Assistants, G-Cores, Mecha Volt MX</div>
+                <div className='border border-gray-600 px-2 py-1 border-b-0'>Age</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 col-span-3'>22</div>
+                <div className='border border-gray-600 px-2 py-1'>Race</div>
+                <div className='border border-gray-600 px-2 border-l-0 py-1 col-span-3'>Human</div>
+              </div>
+
+            </div>
+          </div>
+          <div className='w-[500px] hidden sm:block'>
+            <Image alt='optimus' className='object-cover' src={optimus} />
+          </div>
+        </section>
 
       </main>
     </Fragment>
