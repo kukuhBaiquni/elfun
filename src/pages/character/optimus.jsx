fdf85a6755174c60976f808efd0697a3ab67b94e
kukuh <kukuh.baiquni@gmail.com>
2021-09-26 23:52:13 +0700

    fix positioning

--- src/pages/character/optimus.jsx
+++ src/pages/character/optimus.jsx
@@ -25,24 +25,32 @@ export default function Optimus() {
         <meta content='Elsword, MMORPG, Optimus, Metal Heart, Prime Operator, Rose' name='keywords' />
       </Head>
       <main className='text-gray-600 dark:text-gray-400 px-2 h-[2000px]'>
-        <section className='max-w-screen-2xl h-[400px] bg-gradient-to-r from-sky-500 to-rose flex justify-end relative sm:p-3 mb-3'>
+        <section className='max-w-screen-2xl h-[400px] bg-gradient-to-r from-transparent to-rose flex justify-end relative sm:p-3 mb-16 drop-shadow-lg'>
           <Image
             alt='Optimus Skin Cut'
             className='object-cover'
             quality={100}
             src={OptimusCut}
           />
-          <div className='flex items-center absolute left-2 bottom-2 sm:left-3 sm:bottom-3'>
-            <Image alt='Optimus' height={54} src={avatar} width={54} />
-            <h2 className='text-3xl font-semibold text-rose font-titillium ml-3'>Optimus</h2>
+          <div className='flex items-center absolute bottom-3 left-0 py-2 px-6 rounded-r-full bg-gray-900'>
+            <h2 className='text-3xl text-rose font-titillium'>Optimus</h2>
           </div>
         </section>
 
-        <section className='bg-gradient-to-r max-w-7xl dark:from-gray-700 flex from-gray-200 relative justify-between items-center p-3'>
-          <div className='max-w-full sm:max-w-3xl self-start mr-4'>
-            <h3 className='text-2xl text-sky-500 font-semibold'>Basic Information</h3>
-            <div className='sm:max-w-screen-lg overflow-x-auto mt-3'>
-              <div className='grid grid-cols-5 w-[400px] sm:w-full sm:max-w-lg'>
+        <section className='bg-gradient-to-r dark:from-gray-700 from-gray-200 relative p-3 pt-6'>
+          <div className='w-full sm:w-[510px] p-3 border border-gray-600 mt-3'>
+            <Image alt='optimus' className='object-cover' src={optimus} />
+            <div className='flex justify-center'>
+              <button className='bg-rose text-gray-600 py-2 px-3' type='button'>2nd Job</button>
+              <button className='text-rose py-2 px-3 border border-rose' type='button'>Transcendent</button>
+            </div>
+          </div>
+          <div className='max-w-full bg-pink-500'>
+            <div className='absolute -top-6 left-3 dark:bg-gray-700 bg-gray-200 py-2 px-6 drop-shadow-lg'>
+              <h3 className='text-xl text-sky-500 font-semibold'>Basic Information</h3>
+            </div>
+            <div className='overflow-x-auto mt-3'>
+              <div className='grid grid-cols-5 w-[550px] sm:w-full sm:max-w-lg'>
                 <div className='border border-gray-600 px-2 py-1 border-b-0 font-semibold dark:text-gray-300 text-gray-700'>Name</div>
                 <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 col-span-4'>Anna Testarossa (Rose)</div>
                 <div className='border border-gray-600 px-2 py-1 border-b-0 font-semibold dark:text-gray-300 text-gray-700'>Class</div>
@@ -64,40 +72,36 @@ export default function Optimus() {
                 <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 col-span-4'>22</div>
                 <div className='border border-gray-600 px-2 py-1 border-b-0 font-semibold dark:text-gray-300 text-gray-700'>Race</div>
                 <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 col-span-4'>Human</div>
-                <div className='border border-gray-600 px-2 border-b-0 py-1 flex items-center font-semibold dark:text-gray-300 text-gray-700'>Class Tree</div>
+                <div className='border border-gray-600 px-2 border-b-0 py-1 flex items-center font-semibold dark:text-gray-300 text-gray-700 whitespace-nowrap'>Class Tree</div>
                 <div className='border border-gray-600 px-2 border-l-0 py-2 col-span-4 flex border-b-0 justify-between items-center'>
-                  <Image alt='rose' height={40} src={rose} width={40} />
+                  <Image alt='rose' height={40} quality={100} src={rose} width={40} />
                   <ArrowSmRightIcon className='w-5 h-5' />
-                  <Image alt='rose' height={40} src={metalHeart} width={40} />
+                  <Image alt='rose' height={40} quality={100} src={metalHeart} width={40} />
                   <ArrowSmRightIcon className='w-5 h-5' />
-                  <Image alt='rose' height={40} src={avatar} width={40} />
+                  <Image alt='rose' height={40} quality={100} src={avatar} width={40} />
                   <ArrowSmRightIcon className='w-5 h-5' />
-                  <Image alt='rose' height={40} src={optimusT} width={40} />
+                  <Image alt='rose' height={40} quality={100} src={optimusT} width={40} />
                   <ArrowSmRightIcon className='w-5 h-5' />
-                  <Image alt='rose' height={40} src={primeOperator} width={40} />
+                  <Image alt='rose' height={40} quality={100} src={primeOperator} width={40} />
                 </div>
 
                 <div className='border border-gray-600 row-span-3 px-2 py-1 flex items-center font-semibold dark:text-gray-300 text-gray-700'>Statistic</div>
                 <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 font-semibold dark:text-gray-300 text-gray-700'>Speed:</div>
-                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1'>Slow</div>
-                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 bg-rose text-gray-600'>Average</div>
-                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1'>Fast</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 text-center'>Slow</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 bg-rose text-gray-600 text-center'>Average</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 text-center'>Fast</div>
 
                 <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 font-semibold dark:text-gray-300 text-gray-700'>Range:</div>
-                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1'>Short</div>
-                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 bg-rose text-gray-600'>Medium</div>
-                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1'>Long</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 text-center'>Short</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 bg-rose text-gray-600 text-center'>Medium</div>
+                <div className='border border-gray-600 px-2 border-l-0 border-b-0 py-1 text-center'>Long</div>
 
                 <div className='border border-gray-600 px-2 border-l-0 py-1 font-semibold dark:text-gray-300 text-gray-700'>Difficulty:</div>
-                <div className='border border-gray-600 px-2 border-l-0 py-1'>Easy</div>
-                <div className='border border-gray-600 px-2 border-l-0 py-1'>Normal</div>
-                <div className='border border-gray-600 px-2 border-l-0 py-1 bg-rose text-gray-600'>Hard</div>
+                <div className='border border-gray-600 px-2 border-l-0 py-1 text-center'>Easy</div>
+                <div className='border border-gray-600 px-2 border-l-0 py-1 text-center'>Normal</div>
+                <div className='border border-gray-600 px-2 border-l-0 py-1 bg-rose text-gray-600 text-center'>Hard</div>
               </div>
-
             </div>
-          </div>
-          <div className='w-[500px] hidden md:block'>
-            <Image alt='optimus' className='object-cover' src={optimus} />
           </div>
         </section>
 
