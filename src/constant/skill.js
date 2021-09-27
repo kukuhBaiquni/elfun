38cf5073808004f42d1c0e1b882e8de2ca27f2f3
kukuh <kukuh.baiquni@gmail.com>
2021-09-27 22:21:38 +0700

    add example structure for skill

---
+++ src/constant/skill.js
@@ -0,0 +1,23 @@
+/*
+ * SKILL CONSTANT
+ * 1. Character id e.g. 11 for rose
+ * 2. Job Path order. e.g. Rose 000
+ *  -Heavy Barrel 101, Storm Trooper 102, Storm Trooper (T) 10T, Tempest Burster 103
+ *  -Bloodia 201, Crimson Rose 202, Crimson Rose (T) 20T, Black Massacre 203
+ *  -Valkyrie 301, Freyja 302, Freyja (T) 30T, Minerva 303
+ *  -Metal Heart 401, Optimus 402, Optimus (T) 40T, Prime Operator 403
+ * 3. Skill Level e.g. for Hyper Active must be 65
+ * 4. Skill Type. passive 0P, active 0A, special active 0S, hyper active 0H
+ * 5. If skill mod, add M otherwise N
+ * 6. If Locked, add L otherwise U
+ * 7. Split characters with period('.')
+ *
+ * Example (G-0 Battleroid)
+ * 11.402.35.0S.N.U
+ * 11 - Rose
+ * 02 - Optimus(2nd Job)
+ * 35 - Skill Level 35
+ * 0S - Special Active
+ * N - skill is not MOD
+ * U - skill is not locked
+*/
