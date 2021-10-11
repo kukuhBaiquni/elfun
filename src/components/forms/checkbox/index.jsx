dd4ec04c726d88a06916544a81ec1182928e7a82
kukuh <kukuh.baiquni@gmail.com>
2021-10-11 21:42:51 +0700

    Dynamic props

--- src/components/forms/checkbox/index.jsx
+++ src/components/forms/checkbox/index.jsx
@@ -1,12 +1,18 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import PropTypes from 'prop-types'
+import { Fragment } from 'react'
 
 export default function Checkbox(props) {
-  const { name = 'name', label = 'Label' } = props
+  const { name = 'name', label = 'Label', options = [] } = props
   return (
     <div className='py-2 text-general font-titillium'>
       <label className='font-semibold block' htmlFor={name}>{label}</label>
-      <input id='vehicle1' name='vehicle1' type='checkbox' value='Bike' />
-      <label className='ml-2' htmlFor='vehicle1'> I have a bike</label>
+      {options.map((item) => (
+        <Fragment key={item.label}>
+          <input className='rounded p-6 checked:bg-sky-500' id='vehicle1' name='vehicle1' type='checkbox' value='Bike' />
+          <label className='ml-2' htmlFor='vehicle1' />
+        </Fragment>
+      ))}
     </div>
   )
 }
@@ -14,4 +20,5 @@ export default function Checkbox(props) {
 Checkbox.propTypes = {
   name: PropTypes.string,
   label: PropTypes.string,
+  options: PropTypes.array,
 }
