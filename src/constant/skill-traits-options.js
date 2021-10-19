5122b8711a936891802543926dce460fd7088263
kukuh <kukuh.baiquni@gmail.com>
2021-10-19 11:01:36 +0700

    Add character stats constant

--- src/constant/skill-traits-options.js
+++ src/constant/skill-traits-options.js
@@ -1,11 +1,11 @@
 const SKILL_TRAITS_OPTIONS = [
   {
     name: 'Empowered',
-    id: '1.0',
+    id: 'T-1.0',
     effect: [
       {
         description: 'Damage increased',
-        id: '1.1',
+        id: 'T-1.1',
         dynamic: true,
         amount: 0,
       },
@@ -13,11 +13,11 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Gigantic',
-    id: '2.0',
+    id: 'T-2.0',
     effect: [
       {
         description: 'Skill radius/size increased',
-        id: '2.1',
+        id: 'T-2.1',
         dynamic: true,
         amount: 0,
       },
@@ -25,11 +25,11 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Light',
-    id: '3.0',
+    id: 'T-3.0',
     effect: [
       {
         description: 'MP cost reduced',
-        id: '3.1',
+        id: 'T-3.1',
         dynamic: true,
         amount: 0,
       },
@@ -37,35 +37,35 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Heavy',
-    id: '4.0',
+    id: 'T-4.0',
     effect: [
       {
         description: 'Damage increased by 44%',
-        id: '4.1',
+        id: 'T-4.1',
         dynamic: false,
-        amount: 0,
+        amount: 0.4,
       },
       {
         description: 'Cooldown time increased to 120%',
-        id: '4.2',
+        id: 'T-4.2',
         dynamic: false,
-        amount: 0,
+        amount: 1.2,
       },
     ],
   },
   {
     name: 'Useful',
-    id: '5.0',
+    id: 'T-5.0',
     effect: [
       {
         description: 'Damage reduced',
-        id: '5.1',
+        id: 'T-5.1',
         dynamic: true,
         amount: 0,
       },
       {
         description: 'Hit count increased',
-        id: '5.2',
+        id: 'T-5.2',
         dynamic: true,
         amount: 0,
       },
@@ -73,17 +73,17 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Enhanced',
-    id: '6.0',
+    id: 'T-6.0',
     effect: [
       {
         description: 'Critical rate increased to 100%',
-        id: '6.1',
+        id: 'T-6.1',
         dynamic: false,
-        amount: 0,
+        amount: 1,
       },
       {
         description: 'Damage reduced',
-        id: '6.2',
+        id: 'T-6.2',
         dynamic: true,
         amount: 0,
       },
@@ -91,17 +91,17 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Reversed',
-    id: '7.0',
+    id: 'T-7.0',
     effect: [
       {
         description: 'Cooldown increased',
-        id: '7.1',
+        id: 'T-7.1',
         dynamic: true,
         amount: 0,
       },
       {
         description: 'MP cost reduced',
-        id: '7.2',
+        id: 'T-7.2',
         dynamic: true,
         amount: 0,
       },
@@ -109,29 +109,29 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Critical',
-    id: '8.0',
+    id: 'T-8.0',
     effect: [
       {
         description: 'MP cost increased',
-        id: '8.1',
+        id: 'T-8.1',
         dynamic: true,
         amount: 0,
       },
       {
         description: 'Skill will ignore 50% defense',
-        id: '8.2',
+        id: 'T-8.2',
         dynamic: false,
-        amount: 0,
+        amount: 0.5,
       },
     ],
   },
   {
     name: 'Haste',
-    id: '9.0',
+    id: 'T-9.0',
     effect: [
       {
         description: 'Cooldown reduced',
-        id: '9.1',
+        id: 'T-9.1',
         dynamic: true,
         amount: 0,
       },
@@ -139,11 +139,11 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Regenerating 1',
-    id: '10.0',
+    id: 'T-10.0',
     effect: [
       {
         description: 'At certain chance of regaining some of the MP used',
-        id: '10.1',
+        id: 'T-10.1',
         dynamic: true,
         amount: 0,
       },
@@ -151,41 +151,41 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Regenerating 2',
-    id: '11.0',
+    id: 'T-11.0',
     effect: [
       {
         description: '50% chance of reducing cooldown time to 60%',
-        id: '11.1',
+        id: 'T-11.1',
         dynamic: false,
-        amount: 0,
+        amount: 0.6,
       },
     ],
   },
   {
     name: 'Ruthless',
-    id: '12.0',
+    id: 'T-12.0',
     effect: [
       {
         description: 'Skill will cancel Mana Break',
-        id: '12.1',
+        id: 'T-12.1',
         dynamic: false,
         amount: 0,
       },
       {
         description: 'Cooldown time is doubled',
-        id: '12.2',
+        id: 'T-12.2',
         dynamic: false,
-        amount: 0,
+        amount: 2,
       },
     ],
   },
   {
     name: 'Evil',
-    id: '13.0',
+    id: 'T-13.0',
     effect: [
       {
         description: 'Skill will burn MP per hit',
-        id: '13.1',
+        id: 'T-13.1',
         dynamic: true,
         amount: 0,
       },
@@ -193,17 +193,17 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Powerful',
-    id: '14.0',
+    id: 'T-14.0',
     effect: [
       {
         description: 'Super Armor state added to the skill',
-        id: '14.1',
+        id: 'T-14.1',
         dynamic: false,
         amount: 0,
       },
       {
         description: 'Cooldown increased',
-        id: '14.2',
+        id: 'T-14.2',
         dynamic: true,
         amount: 0,
       },
@@ -211,11 +211,11 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Killing Blow 1',
-    id: '15.0',
+    id: 'T-15.0',
     effect: [
       {
         description: "Skill effect's duration increased",
-        id: '15.1',
+        id: 'T-15.1',
         dynamic: true,
         amount: 0,
       },
@@ -223,11 +223,11 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Killing Blow 2',
-    id: '16.0',
+    id: 'T-16.0',
     effect: [
       {
         description: "Skill effect's effectiveness increased",
-        id: '16.1',
+        id: 'T-16.1',
         dynamic: true,
         amount: 0,
       },
@@ -235,11 +235,11 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Absorbing',
-    id: '17.0',
+    id: 'T-17.0',
     effect: [
       {
         description: "Skill's Mana gain increased",
-        id: '17.1',
+        id: 'T-17.1',
         dynamic: true,
         amount: 0,
       },
@@ -247,11 +247,11 @@ const SKILL_TRAITS_OPTIONS = [
   },
   {
     name: 'Persistent',
-    id: '18.0',
+    id: 'T-18.0',
     effect: [
       {
         description: 'Skill will reduce KD per hit',
-        id: '18.1',
+        id: 'T-18.1',
         dynamic: true,
         amount: 0,
       },
