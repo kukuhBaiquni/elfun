d60db25a82c639466f1e2555bf6491a96ec75024
kukuh <kukuh.baiquni@gmail.com>
2021-10-25 19:56:11 +0700

    Create reusable wrapper for input field

--- src/components/forms/input-image/index.jsx
+++ src/components/forms/input-image/index.jsx
@@ -1,74 +1,11 @@
 import clsx from 'clsx'
 import React, { Fragment } from 'react'
-import Dropzone from 'react-dropzone'
+import { useDropzone } from 'react-dropzone'
 
 export default function InputImage() {
   return (
-    <Dropzone
-      accept='.jpg, .jpeg, .png, .gif, .bmp'
-      disabled={false}
-      multiple={false}
-      noClick
-      onDropAccepted={(acceptedFiles) => console.log(acceptedFiles[0])}
-    >
-      {({
-        getRootProps, getInputProps, open, isDragActive, fileRejections,
-      }) => {
-        const fileRejectionsMsg = fileRejections[0]?.errors[0].message
-        // const finalErrorMsg = fileRejectionsMsg || errorMsg
-        console.log('getInputProps', getInputProps())
-        console.log('getRootProps', getRootProps())
-        return (
-          <Fragment>
-            <p>Jncx</p>
-            <input {...getInputProps()} />
-            {/* <div
-              className={clsx(
-                'border border-neutral-5 px-6 py-16 text-center',
-                isDragActive && 'border-primary',
-                label ? 'border-solid' : 'border-dashed',
-                finalErrorMsg && 'border-error',
-              )}
-              {...getRootProps()}
-            >
-              <input {...getInputProps()} />
-              {isProcessing
-                ? <div>⏳ {isCompressing ? 'Compressing' : 'Uploading'} {fileName}</div>
-                : (
-                  <Fragment>
-                    {label
-                      ? (
-                        <button
-                          className='border-2 border-primary text-neutral-black rounded-lg p-5 m-4 font-semibold'
-                          disabled={disabled}
-                          type='button'
-                          onClick={open}
-                        >
-                          <MdAdd className='text-primary text-4xl mx-auto' />
-                          <div className='text-xs'>Add Photo</div>
-                        </button>
-                      )
-                      : (
-                        <Fragment>
-                          <MdFileUpload className='mx-auto w-10 h-10' />
-                          <button
-                            className='btn btn-outline--primary px-12 py-2 m-4 text-base'
-                            disabled={disabled}
-                            ref={innerRef}
-                            type='button'
-                            onClick={open}
-                          >Add File
-                          </button>
-                          <div className='text-neutral-2'>Or drop a file to upload</div>
-                        </Fragment>
-                      )}
-                  </Fragment>
-                )}
-            </div> */}
-            {/* <Message>{finalErrorMsg}</Message> */}
-          </Fragment>
-        )
-      }}
-    </Dropzone>
+    <div className='bg-lime-400'>
+      jncx
+    </div>
   )
 }
--- src/components/forms/input-text/index.jsx
+++ src/components/forms/input-text/index.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import { memo } from 'react'
-import { ExclamationCircleIcon } from '@heroicons/react/solid'
+import { FormField } from '../common'
 
 function InputText(props) {
   const {
@@ -8,23 +8,16 @@ function InputText(props) {
     defaultValue = '',
   } = props
   return (
-    <div className='py-2 font-titillium text-general'>
-      <label className='font-semibold' htmlFor={name}>{label}</label>
-      <div className='flex items-center border-input w-full focus-within:border-input-focus my-1 px-2 font-nunito rounded transition-all duration-300'>
-        <input
-          {...register(name)}
-          className='w-full outline-none placeholder-gray-500 bg-transparent text-sm py-2'
-          defaultValue={defaultValue}
-          id={name}
-          placeholder={placeholder}
-          type='text'
-        />
-      </div>
-      <div className='flex items-center'>
-        <ExclamationCircleIcon className='w-4 h-4 text-red-500 dark:text-red-700' />
-        <span className='text-red-500 dark:text-red-700 ml-1 text-sm' role='alert'>Error Message!</span>
-      </div>
-    </div>
+    <FormField label={label} name={name}>
+      <input
+        {...register(name)}
+        className='w-full outline-none placeholder-gray-500 bg-transparent text-sm py-2'
+        defaultValue={defaultValue}
+        id={name}
+        placeholder={placeholder}
+        type='text'
+      />
+    </FormField>
   )
 }
 
