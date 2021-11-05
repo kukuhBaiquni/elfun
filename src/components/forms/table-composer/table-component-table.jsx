2d37bad0e7839aa97f3ca1839337723e0c7a2d2c
kukuh <kukuh.baiquni@gmail.com>
2021-11-05 16:08:31 +0700

    Separate table view component

---
+++ src/components/forms/table-composer/table-component-table.jsx
@@ -0,0 +1,64 @@
+import clsx from 'clsx'
+import numberFormat from 'utils/number-format'
+import _ from 'lodash/capitalize'
+import PropTypes from 'prop-types'
+import gridClass from './grid-class'
+
+export default function TableComponentTable(props) {
+  const { tableFields } = props
+  return (
+    <section className={clsx(
+      'mt-1 gap-1 grid grid-cols-1 sm:grid-cols-1',
+    )}
+    >
+      {tableFields.map((item) => (
+        <div className='text-center' key={item.fieldName}>
+          <div className='bg-coolGray-400 font-bold dark:bg-warmGray-600 text-white mb-1 py-2'>
+            <p>{item.fieldName}</p>
+          </div>
+          <div className={clsx(
+            'gap-1 grid grid-cols-1',
+            gridClass[item.attributes.length],
+          )}
+          >
+            {item.attributes.map((attr) => (
+              <div className='dark:bg-gray-700 bg-gray-300 p-1 text-sm overflow-hidden' key={attr.attributeName}>
+                <p className='dark:bg-gray-900 bg-gray-100 py-2 mb-1 font-bold whitespace-nowrap overflow-ellipsis'>{attr.attributeName}</p>
+                {attr.hasAwakeningEffect.value ? (
+                  <div className='grid grid-cols-2 gap-1'>
+                    {Object.entries(attr.value).map(([keys]) => (
+                      <p className='dark:bg-gray-900 font-bold bg-gray-100 text-general py-2' key={keys}>
+                        {_(keys)}
+                      </p>
+                    ))}
+                    {Object.entries(attr.valueType?.value === 'FIXED' ? attr.value : attr.valueRange).map(([keys, value]) => (
+                      <p className='bg-coolGray-400 dark:bg-warmGray-600 text-white py-2' key={keys}>
+                        {attr.valueType?.value === 'FIXED' ? (
+                              `${numberFormat(value)}${attr.suffix.value}`
+                        ) : (
+                              `${numberFormat(value[0])}${attr.suffix.value} → ${numberFormat(value[1])}${attr.suffix.value}`
+                        )}
+                      </p>
+                    ))}
+                  </div>
+                ) : (
+                  <p className=' text-white py-7 bg-coolGray-400 dark:bg-warmGray-600'>
+                    {attr.valueType?.value === 'FIXED' ? (
+                      `${attr.value.normal}${attr.suffix.value}`
+                    ) : (
+                      `${attr.valueRange.normal[0]} → ${attr.valueRange.normal[1]}`
+                    )}
+                  </p>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+TableComponentTable.propTypes = {
+  tableFields: PropTypes.array,
+}
--- src/components/forms/table-composer/table-component.jsx
+++ src/components/forms/table-composer/table-component.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 import clsx from 'clsx'
-import { TrashIcon, PencilIcon } from '@heroicons/react/solid'
-import numberFormat from 'utils/number-format'
-import _ from 'lodash/capitalize'
-import gridClass from './grid-class'
+import {
+  TrashIcon, PencilIcon, CodeIcon, TemplateIcon,
+} from '@heroicons/react/solid'
+import TableComponentTable from './table-component-table'
 
 export default function TableComponent(props) {
   const {
     field, openModal, deleteTable,
   } = props
   const { tableName, tableFields } = field
+  const [showAsTable, setShowAsTable] = useState(true)
 
   const calculatePercentage = (val) => {
 
@@ -17,9 +19,29 @@ export default function TableComponent(props) {
 
   return (
     <div className='dark:bg-gray-900 bg-gray-200 p-2 font-titillium mb-2 overflow-x-auto'>
-      <div className='flex justify-between items-center'>
+      <div className='flex justify-between items-center py-1'>
         <h5 className='font-bold'>{tableName}</h5>
         <div className='flex gap-1'>
+          <TemplateIcon
+            className={clsx(
+              'w-5 h-5 cursor-pointer p-0.5 rounded',
+              showAsTable
+                ? 'bg-sky-500 dark:text-gray-900 text-white'
+                : 'text-warmGray-500',
+            )}
+            title='Show as Code'
+            onClick={() => setShowAsTable(true)}
+          />
+          <CodeIcon
+            className={clsx(
+              'w-5 h-5 cursor-pointer p-0.5 rounded',
+              !showAsTable
+                ? 'bg-sky-500 dark:text-gray-900 text-white'
+                : 'text-warmGray-500',
+            )}
+            title='Show as Code'
+            onClick={() => setShowAsTable(false)}
+          />
           <PencilIcon
             className='w-5 h-5 text-sky-500 cursor-pointer dark:hover:bg-gray-800 hover:bg-gray-300 p-0.5 rounded'
             onClick={() => openModal(field)}
@@ -30,55 +52,11 @@ export default function TableComponent(props) {
           />
         </div>
       </div>
-      <section className={clsx(
-        'mt-1 gap-1 grid grid-cols-1 sm:grid-cols-1',
+      {showAsTable ? (
+        <TableComponentTable tableFields={tableFields} />
+      ) : (
+        <div />
       )}
-      >
-        {tableFields.map((item) => (
-          <div className='text-center' key={item.fieldName}>
-            <div className='bg-coolGray-400 dark:bg-warmGray-600 text-white mb-1 py-2'>
-              <p>{item.fieldName}</p>
-            </div>
-            <div className={clsx(
-              'gap-1 grid grid-cols-1',
-              gridClass[item.attributes.length],
-            )}
-            >
-              {item.attributes.map((attr) => (
-                <div className='dark:bg-gray-700 bg-gray-300 p-1 text-sm overflow-hidden' key={attr.attributeName}>
-                  <p className='dark:bg-gray-800 bg-gray-200 py-2 mb-1 whitespace-nowrap overflow-ellipsis'>{attr.attributeName}</p>
-                  {attr.hasAwakeningEffect.value ? (
-                    <div className='grid grid-cols-2 gap-1'>
-                      {Object.entries(attr.value).map(([keys]) => (
-                        <p className='dark:bg-gray-900 font-bold bg-gray-100 text-general py-2' key={keys}>
-                          {_(keys)}
-                        </p>
-                      ))}
-                      {Object.entries(attr.valueType?.value === 'FIXED' ? attr.value : attr.valueRange).map(([keys, value]) => (
-                        <p className='bg-coolGray-400 dark:bg-warmGray-600 text-white py-2' key={keys}>
-                          {attr.valueType?.value === 'FIXED' ? (
-                              `${numberFormat(value)}${attr.suffix.value}`
-                          ) : (
-                              `${numberFormat(value[0])}${attr.suffix.value} → ${numberFormat(value[1])}${attr.suffix.value}`
-                          )}
-                        </p>
-                      ))}
-                    </div>
-                  ) : (
-                    <p className=' text-general py-7 dark:bg-gray-900 bg-gray-200'>
-                      {attr.valueType?.value === 'FIXED' ? (
-                        `${attr.value.normal}${attr.suffix.value}`
-                      ) : (
-                        `${attr.valueRange.normal[0]} → ${attr.valueRange.normal[1]}`
-                      )}
-                    </p>
-                  )}
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </section>
     </div>
   )
 }
