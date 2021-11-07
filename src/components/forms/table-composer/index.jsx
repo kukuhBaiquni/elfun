b079148bebc3e058e49b89b95fc6983cd6de8a45
kukuh <kukuh.baiquni@gmail.com>
2021-11-07 19:31:02 +0700

    fix number on table display

--- src/components/forms/table-composer/index.jsx
+++ src/components/forms/table-composer/index.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import Button from 'components/common/button'
 import {
-  PencilIcon, SwitchVerticalIcon, TemplateIcon, TrashIcon,
+  PencilIcon, TemplateIcon, TrashIcon,
 } from '@heroicons/react/solid'
 import PropTypes from 'prop-types'
 import { useFieldArray } from 'react-hook-form'
 import Modal from 'components/common/modal'
 import _ from 'lodash/get'
-import Collapse from 'components/common/collapse'
 import TableInformation from 'components/common/table/table-information-skill'
 import { FormFieldWrapper } from '../FormFieldWrapper'
 import TableComposerBodyForm from './table-composer-body-form'
@@ -42,8 +41,9 @@ export default function TableComposer(props) {
     <FormFieldWrapper errorMessage={errorMessage} label={label} name={name}>
       <div className='mb-2'>
         {fields.map((field, index) => (
-          <Collapse
-            additionalToolbar={(
+          <div key={field.$id}>
+            <div className='p-2 dark:bg-gray-900 bg-gray-300 flex items-center justify-between max-w-xl'>
+              <h6 className='uppercase font-bold'>{`${field.tableName}`}</h6>
               <div className='flex gap-1'>
                 <PencilIcon
                   className='w-5 h-5 text-sky-500 cursor-pointer dark:hover:bg-gray-800 hover:bg-gray-300 p-0.5 rounded'
@@ -58,15 +58,11 @@ export default function TableComposer(props) {
                   onClick={() => remove(index)}
                 />
               </div>
-            )}
-            key={field.$id}
-            preIcon={<SwitchVerticalIcon className='h-5 w-5 handle cursor-grab' />}
-            title={`${field.tableName}`}
-          >
-            <div className='overflow-x-auto p-2'>
+            </div>
+            <div className='overflow-x-auto'>
               <TableInformation data={field.tableFields} />
             </div>
-          </Collapse>
+          </div>
         ))}
       </div>
       <Button
