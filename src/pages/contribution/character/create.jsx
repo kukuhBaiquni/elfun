039cdbe685a381767f74fa684f8f339a89f114f3
kukuh <kukuh.baiquni@gmail.com>
2021-11-11 17:19:01 +0700

    prepare for character creation

--- src/pages/contribution/character/create.jsx
+++ src/pages/contribution/character/create.jsx
@@ -1,16 +1,18 @@
-import { Fragment, useState, useEffect } from 'react'
+import { Fragment, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from 'layouts'
 import { useRouter } from 'next/router'
 import Modal from 'components/common/modal'
 import Warning from 'components/common/modal/feedback/warning'
+import { baseClass } from 'constant/characters'
 
 export default function Create() {
-  const [isVisible, setIsVisible] = useState(true)
+  const [isVisible, setIsVisible] = useState(false)
   const { query } = useRouter()
   console.log(query)
-
+  const baseCharacter = baseClass.find((cls) => cls.characterId === +query.characterId)
+  console.log(baseCharacter)
   return (
     <Fragment>
       <Head>
@@ -18,7 +20,7 @@ export default function Create() {
         <meta content='Create Character Information' name='description' />
       </Head>
       <main className='text-general px-2'>
-        <h2 className='text-3xl font-semibold dark:text-sky-400 text-sky-600 font-titillium'>Add {query.character} Information</h2>
+        <h2 className='text-3xl font-semibold dark:text-sky-400 text-sky-600 font-titillium'>Add {query.name} Information</h2>
         <p className='my-2'>
           Choose which character you want to add information:
         </p>
