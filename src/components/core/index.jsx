61d5275723e41616d56a9e924e56a1692cd94bba
kukuh <kukuh.baiquni@gmail.com>
2021-11-13 01:38:57 +0700

    add first query

---
+++ src/components/core/index.jsx
@@ -0,0 +1,21 @@
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      retryDelay: 3000,
+      retry: 3,
+    },
+  },
+})
+
+export default function Core({ children }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
