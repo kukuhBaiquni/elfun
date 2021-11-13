00dd64397cb45d4c7d0c7285d972d2823bc9b358
kukuh <kukuh.baiquni@gmail.com>
2021-11-13 14:01:43 +0700

    change to town/dungeon in constant

--- src/components/side-bar/dungeons.jsx
+++ src/components/side-bar/dungeons.jsx
@@ -1,14 +1,25 @@
 import Link from 'next/link'
 import PropTypes from 'prop-types'
 import clsx from 'clsx'
+import DUNGEONS from 'constant/dungeons'
 
-export default function Dungeons({ data }) {
+export default function Dungeons({ townId, town }) {
   const url = (text) => text.toLowerCase().replace(/\s/g, '-')
-
+  const data = DUNGEONS.filter((dungeon) => dungeon.townId === townId)
   return (
-    <ul className='absolute floatig-list min-w-max z-10 hidden group-hover:flex flex-col top-0 left-28 bg-gray-200 dark:bg-gray-700 drop-shadow-xl group-hover:translate-x-10 transition-transform duration-300'>
-      {data.child.map((item) => (
-        <li className={clsx(data.textColor, data.hoverBgColor, data.hoverTextColor || 'hover:text-white dark:hover:text-white', 'job-nesting')} key={item.name}>
+    <ul className={clsx(
+      'absolute floatig-list min-w-max z-10 hidden group-hover:flex flex-col top-0 left-28 bg-gray-200 dark:bg-gray-700',
+      'drop-shadow-xl group-hover:translate-x-10 transition-transform duration-300',
+    )}
+    >
+      {data.map((item) => (
+        <li
+          className={clsx(
+            town.textColor, town.hoverBgColor, town.hoverTextColor || 'hover:text-white dark:hover:text-white',
+            'job-nesting',
+          )}
+          key={item.name}
+        >
           <Link href={`/character/${url(item.name)}`}>
             <a className='py-1 pl-2 pr-4 flex'>
               {item.name}
@@ -21,5 +32,6 @@ export default function Dungeons({ data }) {
 }
 
 Dungeons.propTypes = {
-  data: PropTypes.object,
+  town: PropTypes.object,
+  townId: PropTypes.number,
 }
